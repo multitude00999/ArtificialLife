57055f981ffeb9ca05ee6851111f5fe895191ff2
Surya Pratap Singh Parmar <surya@Suryas-MBP.cable.rcn.com>
2023-01-06 14:01:06 -0600

    camera added

--- simulate.py
+++ simulate.py
@@ -1,3 +1,10 @@
 import pybullet as p
+import time
 physicsClient = p.connect(p.GUI)
+numSteps = 1000
+waitTime = 1/60
+for i in range(numSteps):
+	p.stepSimulation()
+	time.sleep(waitTime)
+	print("step:", i)
 p.disconnect()
