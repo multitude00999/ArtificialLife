2ade9eb7c0c9e3e327ab1f1c029a646f1516120d
Surya Pratap Singh Parmar <surya@Suryas-MBP.cable.rcn.com>
2023-01-29 19:08:02 -0600

    prallel HC speed test

--- constants.py
+++ constants.py
@@ -3,5 +3,5 @@ import numpy as np
 numSteps = 4000
 waitTime = 1/4000
 amplitude, frequency, offset = np.pi/4, 10, 0 
-numberOfGenerations = 20
-populationSize = 20
+numberOfGenerations = 10
+populationSize = 10
