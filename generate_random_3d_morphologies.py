c5dd83d7999d7f829db0afc356ffb84e0bed463c
Surya Pratap Singh Parmar <surya@Suryas-MBP.cable.rcn.com>
2023-02-26 14:52:07 -0600

    more links more simulations

--- generate_random_3d_morphologies.py
+++ generate_random_3d_morphologies.py
@@ -1,8 +1,8 @@
 from solution_auto_3d import SOLUTION_AUTO_3D
 import random
 
-for i in range(5):
-	numlinks = random.randint(3,5)
+for i in range(7):
+	numlinks = random.randint(5,8)
 	solution = SOLUTION_AUTO_3D(0, numlinks)
 	solution.Start_Simulation("GUI", "0")
 	solution.Wait_For_Simulation_To_End()
